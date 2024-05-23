32b2e7469423b6aa3c8ecef2f8806a08277e6f4c
Narmatha39 <152671316+Narmatha39@users.noreply.github.com>
2024-05-23 10:48:25 +0530

    Update example-changelog.sql

--- example-changelog.sql
+++ example-changelog.sql
@@ -2,7 +2,7 @@
 
 --changeset your.name:1 labels:example-label context:example-context
 --comment: example comment
-CREATE TABLE `business_type` (
+CREATE TABLE `business_types` (
   `id` int(11) NOT NULL,
   `created_date` datetime NOT NULL DEFAULT current_timestamp(),
   `modified_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -11,8 +11,19 @@ CREATE TABLE `business_type` (
   `name` varchar(50) NOT NULL,
   `kyc_doc_id_list` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Dumping data for table `business_types`
+--
 
---rollback DROP TABLE business_type;
+INSERT INTO `business_types` (`id`, `created_date`, `modified_date`, `created_by`, `modified_by`, `name`, `kyc_doc_id_list`) VALUES
+(1, '2022-12-13 06:27:54', '2022-12-13 06:27:54', 1, 1, 'Proprietorship', '1,2,3'),
+(2, '2022-12-13 06:27:54', '2022-12-13 06:27:54', 1, 1, 'Partnership', '1,2,3'),
+(3, '2022-12-13 06:27:54', '2022-12-13 06:27:54', 1, 1, 'Pvt. Ltd', '1,2,3'),
+(4, '2022-12-13 06:27:54', '2022-12-13 06:27:54', 1, 1, 'Ltd. Co', '1,2,3'),
+(5, '2022-12-13 06:27:54', '2022-12-13 06:27:54', 1, 1, 'Others', '1,2,3'),
+(6, '2022-12-13 06:27:54', '2022-12-13 06:27:54', 1, 1, 'Undefined', '0');
+
+--rollback DROP TABLE business_types;
 
 
 
